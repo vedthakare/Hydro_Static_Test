56966ede446a6bc77e8159c0eda50c2ede95ec10
LarryMalary <98489403+LarryMalary@users.noreply.github.com>
2025-02-20 16:11:40 -0500

    Added comma splitting for sensors

--- serial_node.py
+++ serial_node.py
@@ -7,13 +7,20 @@ import time
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 rospy.init_node('serial_node')
 pub = rospy.Publisher('local_sensor_data', Int32, queue_size=10)
+pub2 = rospy.Publisher('local_sensor_data_2', Int32, queue_size=10)
 
 def read_serial():
     while not rospy.is_shutdown():
         try:
             if ser.in_waiting > 0:
                 line = ser.readline().decode('utf-8').strip()
-                pub.publish(int(line))
+                values = line.split(',')  # Split the string at comma
+                if len(values) == 2:  # Make sure we got both values
+                    sensor1_value = int(values[0])
+                    sensor2_value = int(values[1])
+                    # Now you can use sensor1_value and sensor2_value
+                    pub.publish(sensor1_value)
+                    pub2.publish(sensor2_value)
         except UnicodeDecodeError:
             rospy.logwarn("Serial decode error")
         except ValueError:
