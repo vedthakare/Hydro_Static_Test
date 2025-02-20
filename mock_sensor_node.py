b696f6b66567854e2bfe59fa168885f2a34e42a0
LarryMalary <98489403+LarryMalary@users.noreply.github.com>
2025-02-20 10:31:10 -0500

    added a test case

---
+++ mock_sensor_node.py
@@ -0,0 +1,23 @@
+import rospy
+from std_msgs.msg import Int32
+import random
+import math
+
+rospy.init_node('mock_sensor')
+pub = rospy.Publisher('local_sensor_data', Int32, queue_size=10)
+rate = rospy.Rate(10)  # 10Hz
+
+while not rospy.is_shutdown():
+    # Choose one test pattern:
+    
+    # 1. Random values (0-1023)
+    value = random.randint(0, 1023)
+    
+    # 2. Sine wave (simulate analog sensor)
+    # value = int(512 * math.sin(rospy.get_time()) + 512)
+    
+    # 3. Incrementing counter
+    # value = int(rospy.get_time() * 10) % 1024
+    
+    pub.publish(value)
+    rate.sleep() 
