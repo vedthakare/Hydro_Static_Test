98309dca17c87a14b86e1e9c8040c3b2e77a32b9
vedthakare <vedthakare@virginia.edu>
2025-02-25 12:21:52 -0500

    Serial node now reads float values

--- serial_node.py
+++ serial_node.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import rospy
 import serial
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 import time
 
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 rospy.init_node('serial_node')
-pub = rospy.Publisher('local_sensor_data', Int32, queue_size=10)
-pub2 = rospy.Publisher('local_sensor_data_2', Int32, queue_size=10)
+pub = rospy.Publisher('voltage_sensor_data', Float32, queue_size=10)
+pub2 = rospy.Publisher('voltage_sensor_data_2', Float32, queue_size=10)
 
 def read_serial():
     # Send start signal
@@ -26,14 +26,14 @@ def read_serial():
                     
                 values = line.split(',')
                 if len(values) == 2:
-                    sensor1_value = int(values[0])
-                    sensor2_value = int(values[1])
+                    sensor1_value = float(values[0])
+                    sensor2_value = float(values[1])
                     pub.publish(sensor1_value)
                     pub2.publish(sensor2_value)
         except UnicodeDecodeError:
             rospy.logwarn("Serial decode error")
         except ValueError:
-            rospy.logwarn("Invalid integer conversion")
+            rospy.logwarn("Invalid float conversion")
         except serial.SerialException:
             rospy.logerr("Serial port error")
             break
